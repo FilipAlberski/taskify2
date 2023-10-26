8bd54e413da8f8518c7987ff431c464f372904f8
Filip Alberski <alberskif@gmail.com>
2023-10-26 12:46:34 +0200

    test

--- server/src/config/connectDB.ts
+++ server/src/config/connectDB.ts
@@ -1,23 +1,7 @@
 import logger from '../utils/logger';
-import { Sequelize } from 'sequelize';
 
 const DB_URI = process.env.DB_URI || '';
 
-const sequelize = new Sequelize(DB_URI, {
-  dialect: 'postgres',
-  logging: (msg) => logger.info(msg), // Integrate Sequelize logging with your Winston logger
-});
-
-const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    logger.info(
-      'Connection to the database has been established successfully.'
-    );
-  } catch (error) {
-    logger.error('Unable to connect to the database:', error);
-    process.exit(1); // Exit process with a failure code
-  }
-};
+const connectDB = async () => {};
 
 export default connectDB;
