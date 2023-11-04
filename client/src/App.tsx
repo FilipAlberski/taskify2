6928ddd8f3eb52cdd09aa75a19fef2ad4ffda136
Filip Alberski <alberskif@gmail.com>
2023-11-04 18:01:18 +0100

    testing routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
-
+import { Button } from '@mui/material';
+import { Outlet } from 'react-router-dom';
 function App() {
-  return <>test</>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<div>About</div>} />
+      <Route path="/contact" element={<div>Contact</div>} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
