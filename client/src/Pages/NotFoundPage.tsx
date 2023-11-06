a4260162ef7a0e4d2d2df6b8863643f4bc496045
Filip Alberski <alberskif@gmail.com>
2023-11-06 14:03:52 +0100

    fixing notFoundPage

--- client/src/Pages/NotFoundPage.tsx
+++ client/src/Pages/NotFoundPage.tsx
@@ -6,40 +6,35 @@ const NotFoundPage: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <Container component="main" maxWidth="sm" sx={{ mt: 8, mb: 4 }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100vh',
-          textAlign: 'center',
-        }}
+    <Container
+      component="main"
+      maxWidth="sm"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <Typography variant="h1" component="h1" gutterBottom>
+        404
+      </Typography>
+      <Typography variant="h5" component="h2" gutterBottom>
+        Oops! Page not found.
+      </Typography>
+      <Typography variant="body1" color="textSecondary" gutterBottom>
+        The page you are looking for might have been removed, had its
+        name changed, or is temporarily unavailable.
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => navigate('/')}
+        sx={{ mt: 3 }}
       >
-        <Typography variant="h1" component="h1" gutterBottom>
-          404
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          Oops! Page not found.
-        </Typography>
-        <Typography
-          variant="body1"
-          color="textSecondary"
-          gutterBottom
-        >
-          The page you are looking for might have been removed, had
-          its name changed, or is temporarily unavailable.
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => navigate('/')}
-          sx={{ mt: 3 }}
-        >
-          Go to Homepage
-        </Button>
-      </Box>
+        Go to Homepage
+      </Button>
     </Container>
   );
 };
