5247176c544aedd7332a0080d2449f5c206972de
Filip Alberski <alberskif@gmail.com>
2023-11-09 15:51:51 +0100

    Refactor SharedLayout component to use Material-UI components and add Redux integration.

--- client/src/components/SharedLayout.tsx
+++ client/src/components/SharedLayout.tsx
@@ -1,102 +1,191 @@
-import React, { useState, useCallback } from 'react';
-import {
-  useTheme,
-  useMediaQuery,
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  Drawer,
-  List,
-  ListItemButton,
-  ListItemText,
-  styled,
-} from '@mui/material';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close'; // Import Close icon
-import { Outlet } from 'react-router-dom';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
-const drawerWidth = 250;
+//redux
+import { useSelector } from 'react-redux';
 
-const MainContent = styled('div')(({ theme, isMobile }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  marginLeft: isMobile ? 0 : drawerWidth,
-}));
+const drawerWidth = 240;
 
-const SharedLayout: React.FC = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  const [openDrawer, setOpenDrawer] = useState(false);
+interface Props {
+  window?: () => Window;
+}
 
-  const toggleDrawer = useCallback(() => {
-    setOpenDrawer(!openDrawer);
-  }, [openDrawer]);
+export default function SharedLayout(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const closeDrawer = useCallback(() => {
-    setOpenDrawer(false);
-  }, []);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
-  return (
-    <div>
-      <AppBar position="static">
-        <Toolbar
-          sx={{
-            left: isMobile ? '0px' : `${drawerWidth}px`,
-            width: isMobile
-              ? '100%'
-              : `calc(100% - ${drawerWidth}px)`,
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={toggleDrawer}
-          >
-            {openDrawer ? <CloseIcon /> : <MenuIcon />}{' '}
-            {/* Toggle between icons */}
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Taskify
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        open={isMobile ? openDrawer : true}
-        variant={isMobile ? 'temporary' : 'permanent'}
-        onClose={closeDrawer}
-        sx={{
-          width: drawerWidth,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-            bgcolor: 'background.default',
-          },
-        }}
-      >
-        <Typography variant="h4" sx={{ p: 2 }}>
+  const { userInfo } = useSelector((state) => state.auth);
+
+  console.log(userInfo);
+
+  const drawer = (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h6" noWrap component="div">
           Taskify
         </Typography>
-        <List>
-          <ListItemButton onClick={closeDrawer}>
-            <ListItemText primary="Option 1" />
-          </ListItemButton>
-          <ListItemButton onClick={closeDrawer}>
-            <ListItemText primary="Option 2" />
-          </ListItemButton>
-          <ListItemButton onClick={closeDrawer}>
-            <ListItemText primary="Option 3" />
-          </ListItemButton>
-        </List>
-      </Drawer>
-      <MainContent isMobile={isMobile}>
-        <Outlet />
-      </MainContent>
-    </div>
+      </Toolbar>
+      <Divider />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map(
+          (text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          )
+        )}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon onClick={handleDrawerToggle}>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Box
+        sx={{
+          justifySelf: 'flex-end',
+        }}
+      >
+        {userInfo ? (
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Logout" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        ) : (
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Login" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        )}
+      </Box>
+    </Box>
   );
-};
 
-export default SharedLayout;
+  // Remove this const when copying and pasting into your project.
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      {/* Top Bar */}
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      >
+        {/* Side Bar */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        {/* Page Content */}
+        content
+      </Box>
+    </Box>
+  );
+}
