c005dd594868925d3e9c549701fb661440e34a5d
Filip Alberski <alberskif@gmail.com>
2023-11-19 21:17:27 +0100

    Add mongoose import and define RequestWithUser interface

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -1,8 +1,15 @@
 import User from '../models/userModel';
 import Project from '../models/projectModel';
 import asyncHandler from 'express-async-handler';
+import mongoose from 'mongoose';
 
 import { Request, Response } from 'express';
+
+interface RequestWithUser extends Request {
+  user: mongoose.Document;
+}
+
+//(req as RequestWithUser).user
 
 // @desc    Get all projects
 // @route   GET /api/v1/projects
